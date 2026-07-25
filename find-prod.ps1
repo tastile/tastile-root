30cc3914302354de7896ec2044e95092851bfa38
rebuildup <rebuild.up.up@gmail.com>
2026-07-25 14:34:20 +0900

    del tmp scripts

--- find-prod.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-Write-Host "=== EC2 instances tagged tastile ==="
-aws ec2 describe-instances --filters "Name=tag:Name,Values=*tastile*" --query 'Reservations[].Instances[].{Id:InstanceId,Name:Tags[?Key==`Name`]|[0].Value,State:State.Name,Ip:PublicIpAddress,LaunchTime:LaunchTime}' --output table --region ap-northeast-1
-
-Write-Host "=== All EC2 instances (compact) ==="
-aws ec2 describe-instances --query 'Reservations[].Instances[].{Id:InstanceId,State:State.Name,Ip:PublicIpAddress,LaunchTime:LaunchTime,Name:Tags[?Key==`Name`]|[0].Value}' --output table --region ap-northeast-1 2>&1 | Select-Object -First 30
